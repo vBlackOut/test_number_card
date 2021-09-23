c740914ec9ae237373a2750733187c762f2c62dc
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2021-09-24 01:06:02 +0200

    Delete print

--- luhn.py
+++ luhn.py
@@ -14,21 +14,17 @@ class LuhnFormula():
         Check if the input string is valid
         """
 
-        print(self.input[::-1])
         total_chiffre = 0
 
         for i, value in enumerate(self.input[::-1]):
 
             if i % 2 == True:
                 if int(value)*2 >= 10:
-                    #print("somme chiffre", int(value), int(value))
                     somme_chiffre = 0
                     for i in str(int(value)*2):
                         somme_chiffre += int(i)
-                    print("somme chiffre", int(value), int(value)*2, somme_chiffre)
                     total_chiffre += somme_chiffre
                 else:
-                    print("double normal", int(value), int(value)*2)
                     total_chiffre += int(value)*2
 
             else:
@@ -39,7 +35,6 @@ class LuhnFormula():
         print(total_chiffre, total_chiffre%10)
 
         if total_chiffre % 10:
-            print("not ten modulo")
             return False
         else:
             return True
